09da927a133e31144ed03bfeb8dcb775c849cd27
Abhishek Ranjan <abhi03iiitm@gmail.com>
2018-01-11 16:35:29 +0530

    'weather1'

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -4,22 +4,22 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class WeatherProvider {
-
+apikey;
 
   constructor(public http: Http) {
-   
+   this.apikey='0r7cJb6eMKG0XSKs2kpwwE2c9883a0r1';
   }
 
 
    getLocations(value) {
      
-      return  this.http.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=0r7cJb6eMKG0XSKs2kpwwE2c9883a0r1&q='+value)
+      return  this.http.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey='+this.apikey+'&q='+value)
       .map(res=>res.json());
       }
 
-      getDetails(key){
-       console.log(key);
-    return  this.http.get('http://dataservice.accuweather.com/currentconditions/v1/'+key+'?apikey=0r7cJb6eMKG0XSKs2kpwwE2c9883a0r1&details=true')
+      getDetails(locationkey){
+       
+    return  this.http.get('http://dataservice.accuweather.com/currentconditions/v1/'+locationkey+'?apikey='+this.apikey+'&details=true')
       .map(res=>res.json());
 
       }
